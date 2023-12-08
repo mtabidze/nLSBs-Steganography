4f7751d990afa4e959267922ea7493f886195515
Mikheil Tabidze <m.tabidze@gmail.com>
2023-12-08 00:28:29 -0500

    Optimized insertion function with numpy

--- src/nlsbs_steganography/nlsbs_steganography.py
+++ src/nlsbs_steganography/nlsbs_steganography.py
@@ -32,21 +32,26 @@ def insertion(image: Image, message: str, bits_to_use: int = 1) -> Image:
     if min_required_pixels > total_image_pixels:
         raise InsufficientPixelsError("Message size exceeds image pixels capacity.")
 
+    image_array = np.array(image, dtype=np.uint8)
+    image_mode = image.mode
+
     index = 0
     for y in range(image.height):
         for x in range(image.width):
-            pixel = list(image.getpixel(xy=(x, y)))
+            pixel = image_array[y, x, :].tolist()
             for bit_index in range(bits_to_use):
                 new_bit_value = int(binary_message[index], 2)
                 color_value = pixel[-1]
                 clear_bit_color = color_value & ~(1 << bit_index)
                 new_color_value = clear_bit_color | (new_bit_value << bit_index)
                 pixel[-1] = new_color_value
-                image.putpixel(xy=(x, y), value=tuple(pixel))
+                image_array[y, x, :] = pixel
                 index += 1
                 if index >= binary_message_length:
-                    return image
-    return image
+                    new_image = Image.fromarray(obj=image_array, mode=image_mode)
+                    return new_image
+    new_image = Image.fromarray(obj=image_array, mode=image_mode)
+    return new_image
 
 
 def extraction(image: Image, bits_to_use: int = 1) -> str:
